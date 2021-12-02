0ae744ba3e2455d40498774bd96701c26cb2d76f
Joseph McClintock <jmccli9@student.wgu.edu>
2021-12-01 19:50:48 -0800

    Changed fightapp.cpp to main.cpp and fixed the name of the battleSequence function.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
     fighterRoster->parseFightersAndMoves();
 
 
-    fighterRoster->fightingFightStuff();
+    fighterRoster->battleSequence();
 
     //fighterRoster->displayChosenFighter();
 
